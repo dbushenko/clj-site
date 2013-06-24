bc0e8dbcdfbc5d3d609ab69eb4227e8a75a960a8
dbushenko <d.bushenko@gmail.com>
2013-06-24 23:18:36 +0300

    Added RSS generation

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [hiccup "1.0.3"]
-                 [markdown-clj "0.9.25"]]
+                 [markdown-clj "0.9.25"]
+                 [clj-rss "0.1.3"]]
   :main clj-site.core
   :aot :all)
